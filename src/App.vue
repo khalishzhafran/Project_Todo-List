5f5c96eaf258bae2e772c9f9ee60dfe768ccabaa
Khalish-Litsch <khalish260303@gmail.com>
2022-04-16 13:45:05 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  import TodoList from './components/TodoList.vue'
+
+  export default {
+    components: {
+      TodoList
+    },
+  }
+</script>
+
+<template>
+  <main>
+    <TodoList />
+  </main>
+</template>
+
+<style>
+  body {
+    font-family: sans-serif;
+    background-color: rgb(38, 35, 54);
+    color: #fff;
+    box-sizing: border-box;
+  }
+  main {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    data() {
+        return {
+            todo: '',
+            todos: [],
+        }
+    },
+    methods: {
+        saveTodo() {
+            this.todos.push(this.todo);
+            this.todo = '';
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<template>
+
+<section>
+    <section class="todo">
+        <h1 class="title">Todo List App</h1>
+        <p>Create a list of task</p>
+        <div class="action">
+            <input class="input" v-model="todo">
+            <button class="save" @click="saveTodo()">Save</button>
+        </div>
+    </section>
+
+    <section class="list">
+        <div class="task" v-for="(todo, index) in todos">
+            <div class="content">{{ todo }}</div>
+            <button class="delete" @click="deleteTodo(index)">Delete</button>
+        </div>
+    </section>
+</section>
+
+</template>
+
+<style scoped>
+    .title {
+        font-size: 50px;
+        margin-bottom: 10px;
+    }
+
+    .action {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .input {
+        border: none;
+        outline: none;
+        border-bottom: 1px solid #fff;
+        background-color: transparent;
+        color: #fff;
+        font-size: 25px;
+        padding: 10px 0;
+    }
+</style>
